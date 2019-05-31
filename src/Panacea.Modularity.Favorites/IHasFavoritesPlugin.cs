9c91bf356f66a024dea43fceef7286abfa3a9949
Kostas Aronis <k.aronis@dotbydot.gr>
2019-05-31 12:19:27 +0300

    initial push

---
+++ src/Panacea.Modularity.Favorites/IHasFavoritesPlugin.cs
@@ -0,0 +1,10 @@
+﻿using Panacea.Models;
+using System.Collections.Generic;
+
+namespace Panacea.Modularity.Favorites
+{
+    public interface IHasFavoritesPlugin : IPlugin
+    {
+        List<ServerItem> Favorites { get; set; }
+    }    
+}
