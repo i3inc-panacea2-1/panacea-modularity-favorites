6052f841e39e91533834dc66474412c2a142610b
Kostas Aronis <k.aronis@dotbydot.gr>
2019-06-03 17:19:08 +0300

    IFavoritesPlugin

---
+++ src/Panacea.Modularity.Favorites/IFavoritesPlugin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Favorites
+{
+    public interface IFavoritesPlugin : IPlugin
+    {
+        Task FavoriteNotify(string pluginName, string id);
+        Task FavoriteRemove(string pluginName, string id);
+        List<T> GetFavorites<T>(string pluginName);
+    }
+}
---
+++ src/Panacea.Modularity.Favorites/PanaceaServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Panacea.Core;
+using System.Linq;
+
+namespace Panacea.Modularity.Favorites
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IFavoritesPlugin GetFavoritesPlugin(this PanaceaServices core)
+        {
+            return core.PluginLoader.GetPlugin<IFavoritesPlugin>();
+        }
+
+        public static bool TryGetFavoritesPlugin(this PanaceaServices core, out IFavoritesPlugin plugin)
+        {
+            plugin = null;
+            var favorites = core.PluginLoader.GetPlugins<IFavoritesPlugin>().FirstOrDefault();
+            if (favorites == null)
+            {
+                return false;
+            }
+            plugin = favorites;
+            return true;
+        }
+    }
+}
