00da85cfbc9357138e274af5d82ac65fba34fd02
Kostas Aronis <k.aronis@dotbydot.gr>
2019-06-04 11:36:31 +0300

    FriendlyName & PluginFavorites

---
+++ src/Panacea.Modularity.Favorites/FriendlyNameAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Favorites
+{
+    public class FriendlyNameAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public FriendlyNameAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ src/Panacea.Modularity.Favorites/PluginFavorites.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.Favorites
+{
+    [DataContract]
+    public class PluginFavorites<T>
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "items")]
+        public List<T> Items { get; set; }
+    }
+}
